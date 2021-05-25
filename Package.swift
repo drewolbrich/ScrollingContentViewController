bb542c0ef985701d585deb92cafa715efa35fa69
Drew Olbrich <drew@retroactivefiasco.com>
2021-05-25 07:38:25 -0700

    Update package paths

--- Package.swift
+++ Package.swift
@@ -20,9 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ScrollingContentViewController",
-            dependencies: []),
+            dependencies: [],
+            path: "Source"),
         .testTarget(
             name: "ScrollingContentViewControllerTests",
-            dependencies: ["ScrollingContentViewController"])
+            dependencies: ["ScrollingContentViewController"],
+            path: "Tests")
     ]
 )
